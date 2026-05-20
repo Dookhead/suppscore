89ec85ba25b6c9e57724ddc8b2b309fedcdd4cf1
Lucas Checinski <lucaschecinski@gmail.com>
2026-05-20 21:13:48 +1000

    simplify prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,3 @@
 import { defineConfig } from 'prisma/config'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { Pool } from '@neondatabase/serverless'
 
-export default defineConfig({
-  migrate: {
-    async adapter(env: NodeJS.ProcessEnv) {
-      const pool = new Pool({ connectionString: env.DATABASE_URL! })
-      return new PrismaNeon(pool)
-    }
-  }
-})
+export default defineConfig({})
